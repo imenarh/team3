a1c1f0e1af4e29a1a8fe96019237a831dade71cc
Lennie02 <o.ishimwe1@alustudent.com>
2026-05-18 18:58:55 +0200

    Update

--- Database/database_setup.sql
+++ Database/database_setup.sql
@@ -65,8 +65,8 @@ INSERT INTO transaction_category (name, description) VALUES
 
 
 INSERT INTO user (name, phone_number, account_type, created_at) VALUES 
-('Alice ISHIMWE', '0781234567', 'STANDARD', '2025-03-12 08:22:00'),
-('Bob KABALI', '0787654321', 'STANDARD', '2025-07-19 14:05:00'),
+('Orla ISHIMWE', '0781234567', 'STANDARD', '2025-03-12 08:22:00'),
+('Benito KABALI', '0787654321', 'STANDARD', '2025-07-19 14:05:00'),
 ('Kigali Supermarket', '0789998888', 'MERCHANT', '2025-11-04 09:30:00'),
 ('MoMo Agent 001', '0780001111', 'AGENT', '2026-01-08 07:00:00'),
 ('Charlie HABYARIMANA', '0785554444', 'STANDARD', '2026-04-23 16:45:00');
@@ -74,16 +74,16 @@ INSERT INTO user (name, phone_number, account_type, created_at) VALUES
 
 
 INSERT INTO transaction (reference_id, category_id, amount, fee, status, raw_sms_text, timestamp) VALUES 
-('TXN10001', 1, 5000.00, 100.00, 'SUCCESS', 'You have transferred 5000 RWF to Bob KABALI...', '2025-04-10 08:30:00'),
+('TXN10001', 1, 5000.00, 100.00, 'SUCCESS', 'You have transferred 5000 RWF to Benito KABALI...', '2025-04-10 08:30:00'),
 ('TXN10002', 3, 12500.00, 0.00, 'SUCCESS', 'Payment of 12500 RWF to Kigali Supermarket successful...', '2025-07-22 09:15:00'),
 ('TXN10003', 2, 1000.00, 0.00, 'SUCCESS', 'You have bought 1000 RWF airtime...', '2025-09-05 10:00:00'),
 ('TXN10004', 4, 50000.00, 0.00, 'SUCCESS', 'Cash in of 50000 RWF from Agent 001...', '2026-01-14 11:20:00'),
 ('TXN10005', 1, 15000.00, 300.00, 'FAILED', 'Transfer failed due to insufficient funds.', '2026-03-30 12:05:00');
 
 INSERT INTO user_transactions (user_id, transaction_id, role, balance_after) VALUES 
-(1, 1, 'SENDER', 14900.00),  -- Alice sent TXN1
-(2, 1, 'RECEIVER', 25000.00), -- Bob received TXN1
-(1, 2, 'SENDER', 2400.00),   -- Alice paid Merchant TXN2
+(1, 1, 'SENDER', 14900.00),  -- Orla sent TXN1
+(2, 1, 'RECEIVER', 25000.00), -- Benito received TXN1
+(1, 2, 'SENDER', 2400.00),   -- Orla paid Merchant TXN2
 (3, 2, 'RECEIVER', 150000.00),-- Merchant received TXN2
 (5, 4, 'RECEIVER', 52000.00); -- Charlie received Cash In TXN4
 
